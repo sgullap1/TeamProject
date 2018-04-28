0a771dc1a62a6d2b53d15d3a56e8ef7e812c751f
Tamar006 <38636860+Tamar006@users.noreply.github.com>
2018-04-28 01:25:28 -0400

    Update InternshipMatcherTester.java

--- src/teamproject/InternshipMatcherTester.java
+++ src/teamproject/InternshipMatcherTester.java
@@ -14,18 +14,31 @@ public class InternshipMatcherTester {
     public static void main(String[] args) {
 
         //Create few companies
-        Company companyOne = new Company("Google", "goolge.gmail.com", "Larry Jon");
+        //true is paid internship
+        //false is unpaid internship
+        Company companyOne = new Company("Google", "JSullivan@google.com", "Jack Sullivan");
         companyOne.addInternship("BusinessAnalyst", "IT", true);
 
-        Company companyTwo = new Company("Facebook", "facebook.gmail.com", "Mark Zuk");
+        Company companyTwo = new Company("Apple", "ASmith3@apple.com", "Allie Smith");
         companyTwo.addInternship("Programer", "IT", false);
 
-        Company companyThree = new Company("Twitter", "twitter.gmail.com", "Twit Jon");
-        companyThree.addInternship("Payroll", "HR", false);
+        Company companyThree = new Company("Microsoft", "JXMatthews@microsoft.com", "James Matthews");
+        companyThree.addInternship("Web & Content Development", "IT", true);
+        
+        Company companyFour = new Company("Samsung", "Mboseman@samsung.com", "Michelle Bosemane");
+        companyFour.addInternship("Database Administrator", "IT", false);
+        
+        Company companyFive = new Company("Scratch", "STaft@scratch.mit.edu", "Susan Taft");
+        companyFive.addInternship("Video & Graphic Developer", "HR", false);
+        
+        Company companySix = new Company("Scratch", "CXMartin@scratch.mit.edu", "Carrie Martin");
+        companySix.addInternship("Payroll", "HR", false);
+        
+        Company companySeven = new Company("Google", "PHYoung@google.com", "Phillip Young");
+        companySeven.addInternship("Product Design", "HR", true);
 
 
         //System.out.println(companyOne.getOfferedInternships());
-
 
         InternshipMatcher matcher = new InternshipMatcher();
         matcher.addCompany(companyOne);
@@ -37,13 +50,17 @@ public class InternshipMatcherTester {
         Student stuTwo = new Student("Roy", "roy@gmail.com", "HR", "SUMMER");
         Student stuThree = new Student("Mat", "mat@gmail.com", "NETWORKING", "SPRING");
         Student stuFour = new Student("Sam", "sam@gmail.com", "IT", "SUMMER");
-        Student stuFive = new Student("Kim", "kim@gmail.com", "FINANCE", "SUMMER");
-
+        Student stuFive = new Student("Berry", "Berry@gmail.com", "FINANCE", "SUMMER");
+        Student stuSix = new Student("Kim", "kim@gmail.com", "FINANCE", "SUMMER");
+        Student stuSeven = new Student("Fresh", "Fresh@gmail.com", "FINANCE", "SUMMER");
+        
         matcher.addStudent(stuOne);
         matcher.addStudent(stuTwo);
         matcher.addStudent(stuThree);
         matcher.addStudent(stuFour);
         matcher.addStudent(stuFive);
+        matcher.addStudent(stuSix);
+        matcher.addStudent(stuSeven);
 
 
        System.out.println ("Get all Internships ::" + matcher.getAllInterships()) ;
