6356aaa4c07d9eacdae9630ae879250ebf61dcf3
Tamar006 <38636860+Tamar006@users.noreply.github.com>
2018-04-29 10:46:53 -0400

    Update InternshipMatcher.java

--- src/teamproject/InternshipMatcher.java
+++ src/teamproject/InternshipMatcher.java
@@ -56,106 +56,36 @@ Semester for Internship: Summer 2018,  Fall 2018, Spring 2019
     }
     
     private void addExistingCompanies() {
-//method to add existing companies
-        companies.add(new Company(
-            "Google",
-            "Jack Sullivan",
-            "JSullivan@google.com",
-            new CompanyInternship(
-                "Information Technology Intern",
-                "Programming",
-                /* paid = */ true,
-                "Fall 2018")));
+//method to add companies
+        companies.add(new Company( "Google", "Jack Sullivan", "JSullivan@google.com", 
+        new CompanyInternship("Information Technology Intern","Programming", /* paid = */ true, "Fall 2018")));
             
-         companies.add(new Company(
-            "Apple",
-            "Allie Smith",
-            "ASmith3@apple.com",
-            new CompanyInternship(
-                "Software Engineering  Intern: Core iOS",
-                "Software Engineering",
-                /* paid = */ false,
-                "Summer 2018")));
+         companies.add(new Company("Apple", "Allie Smith", "ASmith3@apple.com", 
+         new CompanyInternship("Software Engineering  Intern: Core iOS", "Software Engineering", /* paid = */ false, "Summer 2018")));
                 
-         companies.add(new Company(
-            "Microsoft",
-            "James Matthews",
-            "JXMatthews@microsoft.com",
-            new CompanyInternship(
-                "Web & Content Development",
-                "Web Development",
-                /* paid = */ true,
-                "Spring 2019")));
+         companies.add(new Company("Microsoft", "James Matthews", "JXMatthews@microsoft.com",
+         new CompanyInternship("Web & Content Development", "Web Development", /* paid = */ true, "Spring 2019")));
                 
-         companies.add(new Company(
-            "Microsoft",
-            "John Lawrence",
-            "JXLawrence@microsoft.com",
-            new CompanyInternship(
-                "Database Internship",
-                "Database",
-                /* paid = */ false,
-                "Fall 2018" )));
+         companies.add(new Company("Microsoft", "John Lawrence", "JXLawrence@microsoft.com", 
+         new CompanyInternship("Database Internship", "Database", /* paid = */ false, "Fall 2018" )));
                 
-         companies.add(new Company(
-            "Scratch",
-            "Susan Taft",
-            "STaft@scratch.mit.edu",
-            new CompanyInternship(
-                "Video & Graphic Developer",
-                "Video Developer",
-                /* paid = */ true,
-                "Spring 2019")));
+         companies.add(new Company("Scratch", "Susan Taft", "STaft@scratch.mit.edu", 
+         new CompanyInternship("Video & Graphic Developer", "Video Developer", /* paid = */ true, "Spring 2019")));
                 
-         companies.add(new Company(
-            "Scratch",
-            "Carrie Martin",
-            "CXMartin@scratch.mit.edu",
-            new CompanyInternship(
-                "Web Developer",
-                "Web Development",
-                /* paid = */ false,
-                "Fall 2018")));
+         companies.add(new Company("Scratch", "Carrie Martin", "CXMartin@scratch.mit.edu",
+         new CompanyInternship("Web Developer", "Web Development", /* paid = */ false, "Fall 2018")));
 
-         companies.add(new Company(
-            "Google",
-            "Phillip Young",
-            "PHYoung@google.com",
-            new CompanyInternship(
-                "Product Design",
-                "Programming",
-                /* paid = */ false,
-                "Summer 2018")));
+         companies.add(new Company("Google", "Phillip Young", "PHYoung@google.com",
+         new CompanyInternship("Product Design", "Programming", /* paid = */ false, "Summer 2018")));
 
-         companies.add(new Company(
-            "Apple",
-            "Lisa Pope",
-            "LPopeX@hp@apple.com",
-            new CompanyInternship(
-                "Product Design",
-                "Programming",
-                /* paid = */ false,
-                "Summer 2018")));
+         companies.add(new Company("Apple", "Lisa Pope", "LPopeX@hp@apple.com",
+         new CompanyInternship("Product Design", "Programming", /* paid = */ false, "Summer 2018")));
 
-         companies.add(new Company(
-            "Samsung",
-            "Avery Ryan",
-            "ARyan@samsung.com",
-            new CompanyInternship(
-                "Software Engineering/ Co-Op",
-                "Programming",
-                /* paid = */ true,
-                "Spring 2019")));
+         companies.add(new Company("Samsung", "Avery Ryan", "ARyan@samsung.com",
+         new CompanyInternship("Software Engineering/ Co-Op", "Programming", /* paid = */ true, "Spring 2019")));
       
-         companies.add(new Company(
-            "Samsung",
-            "Michelle Boseman",
-            "Mboseman@samsung.com",
-            new CompanyInternship(
-                "IT/ Computer Systems Intern",
-                "Computer Systems",
-                /* paid = */ false,
-                "Summer 2018")));
+         companies.add(new Company("Samsung", "Michelle Boseman", "Mboseman@samsung.com", 
+         new CompanyInternship("IT/ Computer Systems Intern", "Computer Systems", /* paid = */ false, "Summer 2018")));
     }
     
     //method to add company
