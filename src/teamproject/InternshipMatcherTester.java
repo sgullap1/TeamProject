f706a3bbdfc910ab441cf3c7be548fd1fa3ad3c6
Tamar006 <38636860+Tamar006@users.noreply.github.com>
2018-04-29 11:05:03 -0400

    Update InternshipMatcherTester.java

--- src/teamproject/InternshipMatcherTester.java
+++ src/teamproject/InternshipMatcherTester.java
@@ -3,6 +3,18 @@
 // Name:        Tamar McDowell
 // Instructor:  Dr. Lynda Brown
 // Assignment:  Group Project
+/* 
+Internship Tester 
+based on the UML diagram design
+retrieving information for
+companies
+email
+contact
+students
+internships title
+internship paid/upaid
+*/
+
 
 package teamproject;
 
@@ -44,6 +56,10 @@ public class InternshipMatcherTester {
         matcher.addCompany(companyOne);
         matcher.addCompany(companyTwo);
         matcher.addCompany(companyThree);
+        matcher.addCompany(companyFour);
+        matcher.addCompany(companyFive);
+        matcher.addCompany(companySix);
+        matcher.addCompany(companySeven);
 
 
         Student stuOne = new Student("Tom", "tom@gmail.com", "IT", "FALL");
